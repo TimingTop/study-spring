530037f950161d6861628d045aae319b4f063926
EMINXTA <minxiang.tan@ericsson.com>
2016-10-20 14:05:41 +0800

    a

---
+++ src/main/java/com/tim/study/SayHelloConfiguration.java
@@ -0,0 +1,13 @@
+package com.tim.study;
+
+import com.netflix.client.config.IClientConfig;
+
+/**
+ * Created by eminxta on 2016/07/21.
+ */
+
+public class SayHelloConfiguration {
+
+    IClientConfig ribbonClientConfig;
+
+}
--- src/main/java/com/tim/study/UserApplication.java
+++ src/main/java/com/tim/study/UserApplication.java
@@ -3,6 +3,7 @@ package com.tim.study;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
  */
 @RestController
 @SpringBootApplication
+@RibbonClient(name="say-hello",configuration = SayHelloConfiguration.class)
 public class UserApplication {
 
     @Bean
